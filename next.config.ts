f2ca624dea2f1b88697a4d062c288a5036a86c2a
Arun <arunjamdagni2@gmail.com>
2025-11-03 00:13:29 +0530

    write next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode:true,
+  images:{
+    formats:["image/avif",'image/webp']
+  },
+  experimental:{
+    optimizeCss:true,
+    optimizePackageImports:["framer-motion","react-icons"]
+  },
+  compiler:{
+    removeConsole:process.env.NODE_ENV === "production"
+  }
 };
 
 export default nextConfig;
