3977bbe383dd038b5498aa301537fec84c138003
Arun <arunjamdagni2@gmail.com>
2025-11-15 23:25:04 +0530

    changes

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -39,7 +39,7 @@ const params = usePathname();
      text-center bg-linear-to-b from-[#0a0a0a] via-[#111] to-[#0a0a0a] overflow-hidden`}>
       <div className="absolute inset-0 bg-linear-to-t from-black/60 via-transparent to-transparent z-0">
       <Image
-      src={'/images/bg-5.jpg'}
+      src={'/images/bg-6.jpg'}
       alt="banner-image"
       fill
       loading="lazy"
