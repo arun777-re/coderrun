8a810b5750d6152d9ee9aeade441428e8b01a90d
Arun <arunjamdagni2@gmail.com>
2025-11-21 23:20:43 +0530

    changes

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -29,12 +29,11 @@ const Projects = () => {
 
   return (
     <section className="py-10 bg-surface/80 text-center overflow-x-hidden" id="projects">
-      <h2 className="text-3xl md:text-4xl text-accent mb-8" data-aos="fade-up">
+      <h2 className="text-3xl md:text-4xl text-accent mb-6" data-aos="fade-up">
         Some of My Projects
       </h2>
 
       <div className="w-full px-4 md:px-16 h-auto ">
-
         {/* Embla viewport */}
         <div className="embla w-full relative overflow-hidden" ref={emblaRef} >
           <div className="embla__container flex gap-4 ml-2 sm:ml-0 sm:px-4 rounded-lg py-10  ">
@@ -57,18 +56,18 @@ const Projects = () => {
 
 
         </div>
-        <div className="absolute flex gap-4 top-110 z-20 items-center left-1/2 transform -translate-x-1/2">
+        <div className="relative flex gap-4 top-0 justify-center z-20 items-center left-1/2 transform -translate-x-1/2">
           {/* Buttons */}
           <button
             onClick={scrollPrev}
-            className=" bg-accent/20 p-2 rounded-full"
+            className=" bg-accent/20 p-2 rounded-full cursor-pointer w-10 h-10 hover:bg-accent/40 transition-colors duration-300"
           >
             ‹
           </button>
 
           <button
             onClick={scrollNext}
-            className="a bg-accent/20 p-2 rounded-full"
+            className="bg-accent/20 p-2 rounded-full cursor-pointer w-10 h-10 hover:bg-accent/40 transition-colors duration-300"
           >
             ›
           </button>
